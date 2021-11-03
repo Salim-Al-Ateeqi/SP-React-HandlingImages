f6604681ca26d3ce9531cd1c9a4ecf0c3a35aed4
CaptSoulBeard <76257198+CaptSoulBeard@users.noreply.github.com>
2021-11-03 10:30:35 +0300

    FrontEnd Done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,7 +7,7 @@ function NavBar() {
     <Navbar bg="dark" variant="dark">
       <Container>
         <Link to="/products">
-          <Navbar.Brand>Chicken Shop</Navbar.Brand>
+          <Navbar.Brand>Salim's Emporium</Navbar.Brand>
         </Link>
         {/* <Nav className="me-auto">
           <Nav.Link>
